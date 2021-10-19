8f08b39d359d007b9ced2589f91007e1a3d8aa3d
Fathur Rohman <hi.fathur.rohman@gmail.com>
2021-10-19 13:00:59 +0700

    add login validation

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -12,6 +12,11 @@ class AuthenticationController extends Controller
     public function login(Request $request)
     {
         $user = User::find($request->get('customer_xid'));
+
+        if (is_null($user)) {
+            throw new \Exception("Customer id not found,");
+        }
+
         $token = auth()->login($user);
 
         return fractal([
